c59312318e6367dba2c548292e801d22d3faeb1a
poglodjan <100129357+poglodjan@users.noreply.github.com>
2022-03-06 14:18:47 +0100

    Update PracaDomowa1.py

--- PracaDomowa1.py
+++ PracaDomowa1.py
@@ -5,7 +5,7 @@
 # 1. osoba zarabia 3000 złotych, na kino wydaje miesięcznie 25zł
 # 2. osoba zarabia 4200 złotych, na kino wydaje miesięcznie 50zł
 # 3. osoba zarabia 8700 złotych, na kino wydaje miesięcznie 58zł
-# 4. osoba zarabia 1200 złotych, na kino wydaje miesięcznie 120zł
+# 4. osoba zarabia 12000 złotych, na kino wydaje miesięcznie 120zł
 #
 # Pytanie: Ile pieniędzy teorytycznie będziemy wydawać na kino, jeżeli zarabiamy 16000zł?
 #
