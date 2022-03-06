ed79179370b228f09732faaae983bb351f39abb1
poglodjan <100129357+poglodjan@users.noreply.github.com>
2022-03-06 14:18:09 +0100

    Create PracaDomowa1.py

---
+++ PracaDomowa1.py
@@ -0,0 +1,12 @@
+# Zbadamy ile pieniędzy ludzie wydają na kino w zależności od ich miesięcznych zarobków
+# W tym przypadku x-oznacza ilość wynagrodzenia (w zł), y-oznacza ile ta osoba wydaje na kino (w zł)
+# Przeprowadziliśmy ankietę na czterech osobach i dostaliśmy od nich następujące dane:
+#
+# 1. osoba zarabia 3000 złotych, na kino wydaje miesięcznie 25zł
+# 2. osoba zarabia 4200 złotych, na kino wydaje miesięcznie 50zł
+# 3. osoba zarabia 8700 złotych, na kino wydaje miesięcznie 58zł
+# 4. osoba zarabia 1200 złotych, na kino wydaje miesięcznie 120zł
+#
+# Pytanie: Ile pieniędzy teorytycznie będziemy wydawać na kino, jeżeli zarabiamy 16000zł?
+#
+# Na rozwiązanie tego problemu jest czas do następnych zajęć, 10 marca. 
