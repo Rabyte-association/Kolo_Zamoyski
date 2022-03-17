004903bad064cc929e06a25a72e1e978a6faff87
poglodjan <100129357+poglodjan@users.noreply.github.com>
2022-03-17 20:49:55 +0100

    Update macierze.py

--- macierze.py
+++ macierze.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import math
+
 def macierz_startowa(n):
     A = [[0]*n for j in range(n)] #Tworzymy macierz 
     for i in range(n):
@@ -15,6 +18,39 @@ def nowe_grono(A, N):
             if i==j:
                 A[i][j]=1
     return B
+
+def licznosc_stopni(A):
+    n = len(A)
+    stopnie_popularnosci = [sum(A[i]) for i in range(n)]
+    max_deg = max(stopnie_popularnosci)
+    l = [0] * (max_deg+1)
+    for i in stopnie_popularnosci:
+        l[i] += 1
+    return l
+def p(k, m):
+    if k >= m:
+        return math.e**(1-k/m)/m
+    return 0
+
+def bledy():
+    # Pomocnicza funkcja obliczająca błędy w pojedynczej symulacji
+    N = 100
+    m = 3
+    n = 5
+    A = macierz_startowa(n)
+    B = nowe_grono(A, N, m)
+    l = licznosc_stopni(B)
+    bledy = [l[i] - N*p(i, m) for i in range(len(l))]
+    return bledy
+
+def srednie_bledy(T):
+    N = 100
+    wszystkie_bledy = [0 for i in range(N+1)]
+    for i in range(T):
+        nowe_bledy = bledy()
+        for j in range(len(nowe_bledy)):
+            wszystkie_bledy[j] += nowe_bledy[j]
+    return [wszystkie_bledy[i]/T for i in range(len(wszystkie_bledy))]
 
 def main():
     n=5
@@ -44,6 +80,16 @@ def main():
         print("te osoby się znają")
     if B[sprawdz1][sprawdz2]==0:
         print("te osoby się nie znają")
+
+    #mozemy tez sprobowac narysowac rzut naszej macierzy na wykres:
+    dane_x=[]
+    dane_y=[]
+    for i in range(N):
+        for j in range(N):
+            dane_x.append(B[i][0])
+            dane_y.append(B[0][j])
+    plt.scatter(dane_x, dane_y)
+    plt.show()
 
 
 if __name__ == "__main__":
